2fd910cf320230fc228e089fd401f608c4f06c45
JamesBremner <james@ravenspoint.com>
2022-12-29 14:38:56 -0500

    Ass tests

--- src/test.cpp
+++ src/test.cpp
@@ -11,6 +11,37 @@ TEST(test)
     O.connect();
     O.tourSpanningTree();
     CHECK_EQUAL( 15, O.path().size() );
+    CHECK_EQUAL( 0, O.NodesRevisited().size() );
+}
+TEST(farm)
+{
+    cObstacle O;
+    read(O, "../data/farm.txt");
+    O.unobstructedPoints();
+    O.connect();
+    O.tourSpanningTree();
+    CHECK_EQUAL( 99, O.path().size() );
+     CHECK_EQUAL( 0, O.NodesRevisited().size() );
+}
+TEST(farmpoly)
+{
+    cObstacle O;
+    read(O, "../data/farmpoly.txt");
+    O.unobstructedPoints();
+    O.connect();
+    O.tourSpanningTree();
+    CHECK_EQUAL( 88, O.path().size() );
+     CHECK_EQUAL( 0, O.NodesRevisited().size() );
+}
+TEST(farmconcave)
+{
+    cObstacle O;
+    read(O, "../data/farmconcave.txt");
+    O.unobstructedPoints();
+    O.connect();
+    O.tourSpanningTree();
+    CHECK_EQUAL( 82, O.path().size() );
+    CHECK_EQUAL( 1, O.NodesRevisited().size() );
 }
 
 main()
